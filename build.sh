876d63c9a09cdbc729f94f958d819c030d2eb5d8
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-03 20:59:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ mkdir -p llvm-build
 cd llvm-build
 cmake ../llvm-project/llvm -DCMAKE_BUILD_TYPE=Release -G Ninja \
         -DLLVM_ENABLE_PROJECTS="clang;lld" \
-        -DLLVM_TARGETS_TO_BUILD="RISCV" 
+        -DLLVM_TARGETS_TO_BUILD="X86;RISCV" 
 cmake --build . -j
-ctest -j
+cmake --build . -j -t check-all
