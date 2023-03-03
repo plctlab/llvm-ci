100177df3578d3c48e36e13c74bb98d33543af48
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-04 02:38:59 +0800

    emit change logs

--- update-llvm.sh
+++ update-llvm.sh
@@ -5,7 +5,11 @@ then
   cd llvm-project
   git checkout .
   git clean -fdx
+  last_commit=$(git rev-parse HEAD)
   git pull
+  current_commit=$(git rev-parse HEAD)
+  echo "from $last_commit to $current_commit\n" > CHANGELOGS
+  git log $last_commit..HEAD --pretty=oneline >> CHANGELOGS
 else
   git clone https://github.com/llvm/llvm-project
 fi
