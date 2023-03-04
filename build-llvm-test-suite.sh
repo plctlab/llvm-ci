85859c28da21f9392f7b77c1d4502a6fc28816b4
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-04 15:55:06 +0800

    Fix CI

--- build-llvm-test-suite.sh
+++ build-llvm-test-suite.sh
@@ -5,7 +5,8 @@ toolchain=$1
 mkdir -p llvm-test-suite-build-tmp
 cd llvm-test-suite-build-tmp
 flags="-fuse-ld=lld -static -mcpu=sifive-u74"
-cmake -DCMAKE_C_COMPILER=../llvm-build/bin/clang -DCMAKE_C_COMPILER=../llvm-build/bin/clang++ -G Ninja \
+export CLANG_PATH=$PWD/../llvm-build/bin
+cmake -G Ninja \
       -DCMAKE_C_FLAGS="$flags" \
       -DCMAKE_CXX_FLAGS="$flags" \
       -DOPTFLAGS="$flags" \
--- target-riscv64.cmake
+++ target-riscv64.cmake
@@ -1,6 +1,11 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
+set(CMAKE_SYSROOT /usr/riscv64-linux-gnu)
+
+set(CMAKE_C_COMPILER $ENV{CLANG_PATH}/clang)
+set(CMAKE_CXX_COMPILER $ENV{CLANG_PATH}/clang++)
+
 set(triple riscv64-unknown-linux-gnu)
 
 set(CMAKE_C_COMPILER_TARGET ${triple})
