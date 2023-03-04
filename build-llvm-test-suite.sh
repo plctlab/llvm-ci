90a5ca96610ff78878cb7f3f6e139fc9bcaa093a
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-04 15:47:42 +0800

    Fix CI

--- build-llvm-test-suite.sh
+++ build-llvm-test-suite.sh
@@ -6,9 +6,9 @@ mkdir -p llvm-test-suite-build-tmp
 cd llvm-test-suite-build-tmp
 flags="-fuse-ld=lld -static -mcpu=sifive-u74"
 cmake -DCMAKE_C_COMPILER=../llvm-build/bin/clang -DCMAKE_C_COMPILER=../llvm-build/bin/clang++ -G Ninja \
-      -DCMAKE_C_FLAGS=$flags \
-      -DCMAKE_CXX_FLAGS=$flags \
-      -DOPTFLAGS=$flags \
+      -DCMAKE_C_FLAGS="$flags" \
+      -DCMAKE_CXX_FLAGS="$flags" \
+      -DOPTFLAGS="$flags" \
       -DCMAKE_TOOLCHAIN_FILE=$toolchain \
       -C ../llvm-test-suite/cmake/caches/CodeSize.cmake \
       -DTEST_SUITE_BENCHMARKING_ONLY=ON \
