e40d789a01acec1a53e5fe400d2561a0e2d95759
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-04 16:30:58 +0800

    Make builds deterministic

--- build-llvm-test-suite.sh
+++ build-llvm-test-suite.sh
@@ -4,7 +4,8 @@ toolchain=$1
 
 mkdir -p llvm-test-suite-build-tmp
 cd llvm-test-suite-build-tmp
-flags="-fuse-ld=lld -static -mcpu=sifive-u74"
+reproducible_build="-Wno-builtin-macro-redefined -D__DATE__= -D__TIME__= -D__TIMESTAMP__= "
+flags="-fuse-ld=lld -static -mcpu=sifive-u74 $reproducible_build"
 export CLANG_PATH=$PWD/../llvm-build/bin
 cmake -G Ninja \
       -DCMAKE_C_FLAGS="$flags" \
