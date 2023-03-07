bb8739ec52c06c855b6588b283cfe5f558db2c27
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-07 09:03:18 +0800

    Update update-binary-database.py

--- update-binary-database.py
+++ update-binary-database.py
@@ -12,6 +12,7 @@ cnt = 0
 for r, ds, fs in os.walk(src):
     for f in fs:
         path = r + '/' + f
+        print(path)
         if os.access(path, os.X_OK) and f.endswith(".stripped"):
             dst_path = None
             with open(path, "rb") as file:
