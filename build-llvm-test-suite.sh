7fbee1e745ca7b187f63d72438d66bc565cddf23
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-07 20:16:46 +0800

    Update build-llvm-test-suite.sh

--- build-llvm-test-suite.sh
+++ build-llvm-test-suite.sh
@@ -17,7 +17,7 @@ then
   # For LTO: -mllvm -lto-embed-bitcode=optimized
   embed_bitcode="-fembed-bitcode "
   reproducible_build="-Qn -Wno-builtin-macro-redefined -D__DATE__= -D__TIME__= -D__TIMESTAMP__= "
-  flags="-fuse-ld=lld -mcpu=sifive-u74 $embed_bitcode $reproducible_build $PATCH_ADDITIONAL_FLAGS"
+  flags="-fuse-ld=lld -mcpu=sifive-u74 -Wno-unused-command-line-argument $embed_bitcode $reproducible_build $PATCH_ADDITIONAL_FLAGS"
   export CLANG_PATH=$PWD/../llvm-build/bin
   cmake -G Ninja \
         -DCMAKE_C_FLAGS="$flags" \
