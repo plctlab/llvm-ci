bb566dea300ee2bbe60e2486bbf91676879c298d
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-07 16:54:20 +0800

    Add CI for pre-commit patches

---
+++ setup-pre-commit-patch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export PATCH_ID=DXXXXXX
+export PATCH_ADDITIONAL_FLAGS=""
---
+++ setup-work-space.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BASE_DIR = $1
+
+mkdir -p $BASE_DIR
+mkdir -p $BASE_DIR/binaries
+mkdir -p $BASE_DIR/artifacts
+rm -rf $BASE_DIR/artifacts/*
