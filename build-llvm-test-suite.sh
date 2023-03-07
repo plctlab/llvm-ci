a4607229f102110f54dc059f766115ef962d95ff
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-07 10:37:26 +0800

    Update build-llvm-test-suite.sh

--- build-llvm-test-suite.sh
+++ build-llvm-test-suite.sh
@@ -14,7 +14,8 @@ then
   fi
   mkdir llvm-test-suite-build-tmp
   cd llvm-test-suite-build-tmp
-  embed_bitcode="-fembed-bitcode -mllvm -lto-embed-bitcode=optimized "
+  # For LTO: -mllvm -lto-embed-bitcode=optimized
+  embed_bitcode="-fembed-bitcode "
   reproducible_build="-Wno-builtin-macro-redefined -D__DATE__= -D__TIME__= -D__TIMESTAMP__= "
   flags="-fuse-ld=lld -mcpu=sifive-u74 $embed_bitcode $reproducible_build"
   export CLANG_PATH=$PWD/../llvm-build/bin
