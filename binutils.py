460dc03fd43885e65b3c6aa639a89c862a9b8bb7
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-10 14:10:19 +0800

    Add simple CLI for binutils.py

--- binutils.py
+++ binutils.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 import subprocess
 import os
+import sys
 
 
 def dump_asm(filename: str, objdump):
@@ -43,3 +45,26 @@ def extract_bc(filename: str, objcopy, dis):
                 dump_bc(basedir, dis, count, data[last_pos: pos])
             last_pos = pos
             count += 1
+
+
+def check_access(bin):
+    return os.path.exists(bin) and os.access(bin, os.X_OK)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: binutils.py <llvm-bin-path> binaries...')
+        exit(1)
+
+    llvm_path = sys.argv[1].removesuffix('/')
+    llvm_dis = llvm_path + '/llvm-dis'
+    llvm_objdump = llvm_path+"/llvm-objdump"
+    llvm_objcopy = llvm_path+"/llvm-objcopy"
+
+    if not (check_access(llvm_dis) and check_access(llvm_objdump) and check_access(llvm_objcopy)):
+        print('Error: invalid llvm binaries path')
+        exit(1)
+
+    for bin in sys.argv[2:]:
+        dump_asm(bin, llvm_objdump)
+        extract_bc(bin, llvm_objcopy, llvm_dis)
