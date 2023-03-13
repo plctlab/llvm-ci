886709631e682f11b2485544a6e48ebdda3a4475
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-13 11:13:13 +0800

    Update build-llvm.sh

--- build-llvm.sh
+++ build-llvm.sh
@@ -5,6 +5,7 @@ shopt -s inherit_errexit
 mkdir -p llvm-build
 cd llvm-build
 cmake ../llvm-project/llvm -DCMAKE_BUILD_TYPE=Release -G Ninja \
+        -DLLVM_FORCE_ENABLE_STATS=ON \
         -DLLVM_ENABLE_PROJECTS="clang;lld" \
         -DLLVM_TARGETS_TO_BUILD="X86;RISCV" 
 cmake --build . -j
