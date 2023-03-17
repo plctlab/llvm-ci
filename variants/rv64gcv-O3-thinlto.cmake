b0b98049ed7fec5d51d98fa50305d3313fff6aac
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-18 02:07:05 +0800

    Add more variants

---
+++ variants/rv64gcv-O3-thinlto.cmake
@@ -0,0 +1,5 @@
+set(OPTFLAGS "${OPTFLAGS} -march=rv64gcv -O3 -fomit-frame-pointer -flto=thin -DNDEBUG")
+
+set(CMAKE_C_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
