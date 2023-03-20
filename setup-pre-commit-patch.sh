3c9249c0a85a12ee68f075b921385160bcaebbc2
Yingwei Zheng <dtcxzyw@qq.com>
2023-03-20 20:48:39 +0800

    Fix #23

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATCH_ID=DXXXXXX
-#export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
+#export PATCH_ID=DXXXXXX
+export GITHUB_PATCH_ID="luxufan/llvm-project/commit/178903a9bd153a84fb73fdb27f3af6437cdd7b3f"
 export PATCH_ADDITIONAL_FLAGS=""
