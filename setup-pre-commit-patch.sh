f1c7fa2027f324735248adb8763b7fd527ec798e
Yingwei Zheng <dtcxzyw@qq.com>
2023-04-03 23:20:12 +0800

    Update setup-pre-commit-patch.sh

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATCH_ID=D147437
-#export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
+#export PATCH_ID=D147437
+export GITHUB_PATCH_ID="dtcxzyw/llvm-project/commit/e3f661cba6cb51777d1f5900bf92318152dd329a"
 export PATCH_ADDITIONAL_FLAGS=""
