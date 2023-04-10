df3afde1bf2889ab6c46791368ee811182c9d0e1
Yingwei Zheng <dtcxzyw@qq.com>
2023-04-10 19:21:51 +0800

    Update setup-pre-commit-patch.sh

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -2,4 +2,4 @@
 
 export PATCH_ID=D147789
 #export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
-export PATCH_ADDITIONAL_FLAGS=""
+export PATCH_ADDITIONAL_FLAGS=" -mllvm --enable-knowledge-retention=true "
