076bf6a1dc4dd16d8e86d8a6532fcf0471e8635f
Yingwei Zheng <dtcxzyw@qq.com>
2023-04-13 19:56:50 +0800

    test new GVN

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATCH_ID=DXXXXXX
+export PATCH_ID=D148216 # This is an NFC patch
 #export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
-export PATCH_ADDITIONAL_FLAGS=""
+export PATCH_ADDITIONAL_FLAGS=" -mllvm -enable-newgvn"
