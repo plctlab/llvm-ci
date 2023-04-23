111262806500391b36ecd092c2ab671f28ef58b8
LiaoChunyu <chunyu@iscas.ac.cn>
2023-04-23 10:34:21 +0800

    select

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATCH_ID=DXXXXXX
+export PATCH_ID=https://github.com/ChunyuLiao/llvm-project
 #export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
 export PATCH_ADDITIONAL_FLAGS=""
