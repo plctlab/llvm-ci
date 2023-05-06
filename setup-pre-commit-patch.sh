79ec82648328e0682bf3f09a5b676530f4189a0b
Yingwei Zheng <dtcxzyw@qq.com>
2023-05-06 17:56:15 +0800

    Update setup-pre-commit-patch.sh

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #export PATCH_ID=DXXXXXX
-export GITHUB_PATCH_ID="llvm/llvm-project/compare/main...dtcxzyw:llvm-project:i32-range-check-isel"
+export GITHUB_PATCH_ID="llvm/llvm-project/compare/main...dtcxzyw:llvm-project:i32-range-check-v2"
 export PATCH_ADDITIONAL_FLAGS=""
