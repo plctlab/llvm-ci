8f3e89b4a3cb93ab244bafa5ccb69e355cdb330e
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-02-05 15:08:55 +0800

    Test PR78417

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATCH_ID=DXXXXXX
-#export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
-export PATCH_ADDITIONAL_FLAGS=""
+#export PATCH_ID=DXXXXXX
+export GITHUB_PATCH_ID="llvm/llvm-project/pull/78417"
+export PATCH_ADDITIONAL_FLAGS=" -Xclang -target-feature -Xclang +no-ret-addr-stack "
