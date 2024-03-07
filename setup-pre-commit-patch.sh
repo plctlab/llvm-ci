7abd1cc2346768bb95600faf0d58f8c3020c6fc6
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-03-07 16:14:51 +0800

    Test PR84232

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATCH_ID=DXXXXXX
-#export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
+#export PATCH_ID=DXXXXXX
+export GITHUB_PATCH_ID="llvm/llvm-project/pull/84232"
 export PATCH_ADDITIONAL_FLAGS=""
