544aff82b4ad16e08d23d05d1febf7508e6c18fb
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-07-10 21:39:43 +0800

    Test reverting PR89927

--- setup-pre-commit-patch.sh
+++ setup-pre-commit-patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATCH_ID=DXXXXXX
-#export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
+#export PATCH_ID=DXXXXXX
+export GITHUB_PATCH_ID="dtcxzyw/llvm-project/commit/8265233a7ecf1b32ead57adb0581af8df073f52e"
 export PATCH_ADDITIONAL_FLAGS=""
